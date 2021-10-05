c5880644b62518e7aa9e291fe2dbcb6fb37753a9
zitreym <baranovd@ro.ru>
2021-10-05 21:41:24 +0300

    Проект с 4 блоками (2 недели работ) старт

---
+++ js/mainslider.js
@@ -0,0 +1,12 @@
+var elem = document.querySelector('.grid-slider-wrap');
+var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'center',
+    contain: true,
+    prevNextButtons: true,
+    freeScroll: true,
+    wrapAround: true,
+    autoPlay: 3000
+});
+
+
