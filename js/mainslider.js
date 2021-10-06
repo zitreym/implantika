91d2681e696d8457b57d79b336903c2cbc3b3518
zitreym <baranovd@ro.ru>
2021-10-07 01:35:04 +0300

    Добавлен блок с врачами 07.10.2021

--- js/mainslider.js
+++ js/mainslider.js
@@ -9,4 +9,15 @@ var flkty = new Flickity(elem, {
     autoPlay: 3000
 });
 
+var doctors_slide = document.querySelector('.doctors-slide');
+var gegveev = new Flickity(doctors_slide, {
+    // options
+    cellAlign: 'center',
+    contain: true,
+    prevNextButtons: true,
+    freeScroll: true,
+    wrapAround: true,
+    pageDots: false,
+});
+
 
